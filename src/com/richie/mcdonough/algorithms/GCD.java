2073984ce994afa53679e2e40acb31a73b74d2cb
RichieMac79 <richiemac79@gmail.com>
2023-01-07 09:42:17 -0800

    Algorithms

--- src/com/richie/mcdonough/algorithms/GCD.java
+++ src/com/richie/mcdonough/algorithms/GCD.java
@@ -11,21 +11,27 @@ public class GCD {
     }
 
     public static int[] findTheGCD (int[] aValues, int[] bValues) {
+        // Create a new array to hold gcd values
         int[] gcdValues = new int[aValues.length];
-        int i = 0;
 
-        for (int a: aValues) {
+        for( int i = 0; i < aValues.length; i++) {
+            // Get two values from each array
+            int a = aValues[i];
             int b = bValues[i];
-            int gcd = 0;
 
+            // GCD always starts at 1
+            int gcd = 1;
+            // Start looping through each number trying to find greater GCD
             for(int g = 1;g <= a && g <= b; g++) {
+                // Use modulus to find divisors for each
                 if(a % g == 0 && b % g == 0) {
+                    // If we find one then set it to GCD
                     gcd = g;
                 }
             }
 
+            // Finally store the GCD in the array
             gcdValues[i] = gcd;
-            i++;
         }
 
         return gcdValues;
